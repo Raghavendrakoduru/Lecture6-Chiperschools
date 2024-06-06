0ad7e9ff300d5543bee1045246c3971c1fad4315
Raghavendra Koduru <119060193+Raghavendrakoduru@users.noreply.github.com>
2024-06-06 16:09:56 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+/*include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //conditional Statements
+///if //if-else/if-else if-else(if the given condition is true then only print the code)
+    //if else
+     cout<<"Enter the marks:";
+     int a;
+     cin>>a;
+     if (a>90){
+        cout<<"A grade";
+     }
+     else if(a<90 && a>80){
+     cout<<"B";
+     }
+     else if(a<80&&a>70){
+        cout<<"C";
+     }
+     else if(a<70&&a>60){
+     cout<<"D";
+     }
+     else{
+        cout<<"Fail";
+     }
+
+    return 0;
+
+}
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter four numbers:";
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a>b && c>d){
+        cout<<"haha";
+    }
+    else if(a>b && c<d){
+        cout<<"hehe";
+    }else {
+    cout<<"heyhey"*/
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter a number:";
+    int a;
+    cin>>a;
+    switch(a)
+    {
+
+    case 1:
+        cout<<"The number is 1";
+        break;
+    case 5:
+        cout<<"The number is 5";
+        break;
+    default:
+        cout<<"Always printed";
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
